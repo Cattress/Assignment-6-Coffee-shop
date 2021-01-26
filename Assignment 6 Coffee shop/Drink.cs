2d658d221682c4693eca2d02f4838546990b0856
Cattress <priyaphon.vincent@gmail.com>
2021-01-26 13:46:04 +0700

    Add project files.

---
+++ Assignment 6 Coffee shop/Drink.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_6_Coffee_shop
+{
+    class Drink
+    {
+        private string name; //tea   coffee   juice
+        private int price;   //45      50       30
+
+        public Drink(string name, int price)
+        {
+            this.name = name;
+            this.price = price;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public int GetPrice()
+        {
+            return price;
+        }
+    }
+}
---
+++ Assignment 6 Coffee shop/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Assignment_6_Coffee_shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Drink[] drinks = new Drink[10];
+
+            drinks[0] = new Drink("coffee", 50);
+            drinks[1] = new Drink("tea", 45);
+            drinks[2] = new Drink("juice", 30);
+            drinks[3] = new Drink("coffee", 50);
+
+            for (int i = 0; i < drinks.Length; i++)
+            {
+                if (drinks[i] == null)
+                {
+                    Console.WriteLine(i = " has nothing there");
+                }
+                
+            }
+            Console.WriteLine(count);
+            Console.ReadLine();
+
+        }
+    }
+}
